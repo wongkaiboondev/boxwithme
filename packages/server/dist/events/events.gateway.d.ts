61535ba664a0276f54e0f465d3bc1063172d8e4c
Kai Boon <95kai13@gmail.com>
2024-06-05 23:22:26 +0800

    refactor: remove dist folder

--- packages/server/dist/events/events.gateway.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
-export declare class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    server: Server;
-    handleConnection(client: Socket): void;
-    handleDisconnect(client: Socket): void;
-    handleMessage(msg: any): void;
-    handleJoinRoom(msg: any, client: Socket): void;
-}
